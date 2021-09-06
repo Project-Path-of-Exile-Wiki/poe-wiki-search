b30b557bd4333ec3376a1cc76963a7c2baa58634
Austin Cooper <cooperaustinj@gmail.com>
2021-09-06 09:29:58 -0500

    Add contributors. Increment version to 1.0.4

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-EXT_NAME="poe-wiki-search"
-
-# This will run the commands which allow you to create both the chromium-based and FF extension packages.
-mkdir -p package; zip -r -FS ./package/$EXT_NAME.zip *; cd ..;
