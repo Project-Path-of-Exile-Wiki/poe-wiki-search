ca937a7cc856b60f0fa7a35814ac795ba34dbcb7
Veritania <Jesper_ant@hotmail.com>
2021-09-08 19:42:09 +0200

    implement ddg redirect

--- src/background/background.js
+++ src/background/background.js
@@ -1,8 +1,9 @@
-import { redirectFromFandom, redirectFromGoogle } from "./redirects.js"
+import { redirectFromFandom, redirectFromGoogle, redirectFromDdg } from "./redirects.js"
 
 // Any requests beginning with these patterns get intercepted.
 const fandomPattern = "https://pathofexile.fandom.com/wiki/*"
 const googlePattern = "https://*.google.com/search?*q=poe+*"
+const duckduckgoPattern = "https://duckduckgo.com/?*q=poe+*"
 
 // Instruction for the browser to redirect based on pattern.
 // `chrome` used instead of `browser` for compat since Firefox supports
@@ -19,6 +20,14 @@ chrome.webRequest.onBeforeRequest.addListener(
     redirectFromGoogle,
     {
         urls: [googlePattern],
+    },
+    ["blocking"],
+)
+
+chrome.webRequest.onBeforeRequest.addListener(
+    redirectFromDdg,
+    {
+        urls: [duckduckgoPattern],
     },
     ["blocking"],
 )
