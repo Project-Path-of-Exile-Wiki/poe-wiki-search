386496ac82e09c2804fbb8db0dcf17460c5a7168
Tony Tang <tonytang.dev@gmail.com>
2021-09-16 00:34:15 +1000

    Fix recursive redirect pattern bug

--- src/background/background.js
+++ src/background/background.js
@@ -2,7 +2,11 @@ import { redirectFromFandom, redirectFromGoogle } from "./redirects.js"
 
 // Any requests beginning with these patterns get intercepted.
 const fandomPattern = "https://pathofexile.fandom.com/wiki/*"
-const googlePattern = "https://*.google.com/search?*q=*poe*wiki*"
+// These Google patterns are written this way to prevent recursively matching on the redirect destination.
+// Redirects will be to `/search?q=site:poewiki.net` which a naive pattern of `q=*poe*wiki*` will match and continuously redirect for.
+const googlePattern1 = "https://*.google.com/search?*q=*poe+*wiki*"
+const googlePattern2 = "https://*.google.com/search?*q=*poewiki+*"
+const googlePattern3 = "https://*.google.com/search?*q=*+poewiki*"
 
 // Instruction for the browser to redirect based on pattern.
 // `chrome` used instead of `browser` for compat since Firefox supports
@@ -18,7 +22,7 @@ chrome.webRequest.onBeforeRequest.addListener(
 chrome.webRequest.onBeforeRequest.addListener(
     redirectFromGoogle,
     {
-        urls: [googlePattern],
+        urls: [googlePattern1, googlePattern2, googlePattern3],
     },
     ["blocking"],
 )
